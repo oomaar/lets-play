c68e15a5b021dbda1aa567ac936fdeaec0e5990f
oomaar <omaar_5@hotmail.com>
2022-07-09 22:19:57 +0200

    Navbar Responsive

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,17 @@
-import { NavContainer, NavLink, NavLinksList } from "./styledNavbar";
+import {
+  NavContainer,
+  LogoContainer,
+  NavLink,
+  NavLinksList,
+} from "./styledNavbar";
 import Image from "next/image";
 
 export const Navbar = () => {
   return (
     <NavContainer>
-      <div>
+      <LogoContainer>
         <Image src="/favicon.ico" width={50} height={50} objectFit="contain" />
-      </div>
+      </LogoContainer>
       <NavLinksList>
         <NavLink>
           <a>Calculator</a>
--- components/Navbar/styledNavbar.ts
+++ components/Navbar/styledNavbar.ts
@@ -6,11 +6,26 @@ const NavContainer = styled.nav`
   justify-content: space-around;
   padding: 0.5rem 4rem;
   box-shadow: 0px 0px 4px rgba(255, 255, 255, 0.3);
+
+  @media screen and (max-width: 768px) {
+    padding: 0.5rem 1rem;
+    justify-content: space-between;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  @media screen and (max-width: 320px) {
+    width: 30px;
+  }
 `;
 
 const NavLinksList = styled.ul`
   display: flex;
-  gap: 0 2rem;
+  gap: 0rem 2rem;
+
+  @media screen and (max-width: 768px) {
+    gap: 0rem 1rem;
+  }
 `;
 
 const NavLink = styled.li`
@@ -20,6 +35,14 @@ const NavLink = styled.li`
 
   :hover {
     transform: scale(1.1);
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
+
+  @media screen and (max-width: 320px) {
+    font-size: 0.8rem;
   }
 `;
 
