370d2f174de9e7128520a9ec213982a810db3a86
oomaar <omaar_5@hotmail.com>
2022-07-26 17:04:45 +0200

    setup provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
-import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import type { AppProps } from "next/app";
 import { Navbar } from "../components";
 import { GlobalStyle, theme } from "../global/GlobalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Navbar />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </Provider>
   );
 }
