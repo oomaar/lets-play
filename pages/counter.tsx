2d6075d1a69c3183fe0cfdc9b9221c62151aaa06
oomaar <omaar_5@hotmail.com>
2022-07-26 17:12:53 +0200

    dispatch actions

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -1,15 +1,27 @@
 import type { NextPage } from "next";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../app/store";
 import {
   Container,
   SubContainer,
   Title,
 } from "../global/styledPages/styledSkills";
+import { decrement, increment } from "../slices/CounterSlice";
 
 const Counter: NextPage = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <Container>
       <SubContainer>
         <Title>Counter with redux</Title>
+
+        <div>
+          <h1>The value of count is {count}</h1>
+          <button onClick={() => dispatch(increment())}>+</button>
+          <button onClick={() => dispatch(decrement())}>-</button>
+        </div>
       </SubContainer>
     </Container>
   );
